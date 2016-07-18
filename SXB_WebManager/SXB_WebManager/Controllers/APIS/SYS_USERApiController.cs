9c986a96c9f4788ca8d58232a406f0e9de70741c
sephirothwzc <zhanchao.wu@icloud.com>
2016-07-18 15:31:17 +0800

    http.get

---
+++ SXB_WebManager/SXB_WebManager/Controllers/APIS/SYS_USERApiController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SXB_WebManager.Controllers.APIS
+{
+    public class SYS_USERApiController : ApiController
+    {
+    }
+}
---
+++ SXB_WebManager/SXB_WebManager/Controllers/SYS_USERController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using DAO;
+
+namespace SXB_WebManager.Controllers
+{
+    public class SYS_USERController : Controller
+    {
+        // GET: SYS_USER
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult GetSYS_USER()
+        {
+            SYS_USER_DAO dao = new SYS_USER_DAO();
+            var rest = dao.Query();
+            return Json(rest, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
